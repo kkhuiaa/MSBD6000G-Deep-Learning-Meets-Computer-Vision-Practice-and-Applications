634eb5b576d0835eec4164ca87afa2788ebc4161
Jay Hui <kkhuiaa@gmail.com>
2019-11-20 00:32:03 +0800

    no message

--- step2/data.py
+++ step2/data.py
@@ -34,63 +34,6 @@ for file_path in glob.glob(os.path.join(original_folder, '*.jpg')):
     shutil.copy2(fashion_path, os.path.join(folder_dict['train']['5']
         , str(j*total-(j-1) if i==1
             else (j-1)*total+k)+'.jpg'))
-
-    # #second round
-    # j = 2
-    # k = i - j
-    # shutil.copy2(file_path, os.path.join(folder_dict['test']['data'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['test']['1'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['test']['5']
-    #     , str(j*total-(j-1) if i==1
-    #     else j*total-(j-2) if i==2
-    #     else (j-1)*total+k)+'.jpg'))
-
-    # #third round
-    # j = 3
-    # k = i - j
-    # shutil.copy2(file_path, os.path.join(folder_dict['train']['data'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['train']['1'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['train']['5']
-    #     , str(j*total-(j-1) if i==1
-    #     else j*total-(j-2) if i==2
-    #     else j*total-(j-3) if i==3
-    #     else (j-1)*total+k)+'.jpg'))
-    
-    # j = 4
-    # k = i - j
-    # shutil.copy2(file_path, os.path.join(folder_dict['train']['data'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['train']['1'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['train']['5']
-    #     , str(j*total-(j-1) if i==1
-    #     else j*total-(j-2) if i==2
-    #     else j*total-(j-3) if i==3
-    #     else j*total-(j-4) if i==4
-    #     else (j-1)*total+k)+'.jpg'))
-
-    # j = 5
-    # k = i - j
-    # shutil.copy2(file_path, os.path.join(folder_dict['test']['data'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['test']['1'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['test']['5']
-    #     , str(j*total-(j-1) if i==1
-    #     else j*total-(j-2) if i==2
-    #     else j*total-(j-3) if i==3
-    #     else j*total-(j-4) if i==4
-    #     else j*total-(j-5) if i==5
-    #     else (j-1)*total+k)+'.jpg'))
-
-    # j = 6
-    # k = i - j
-    # shutil.copy2(file_path, os.path.join(folder_dict['test']['1'], str((j-1)*total+i)+'.jpg'))
-    # shutil.copy2(fashion_path, os.path.join(folder_dict['test']['5']
-    #     , str(j*total-(j-1) if i==1
-    #     else j*total-(j-2) if i==2
-    #     else j*total-(j-3) if i==3
-    #     else j*total-(j-4) if i==4
-    #     else j*total-(j-5) if i==5
-    #     else j*total-(j-6) if i==6
-    #     else (j-1)*total+k)+'.jpg'))
-
     i += 1
 
     # if i > 1:
